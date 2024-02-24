6bb7d3f3bfc6e7d481411d8d65ddd7a122cdd172
Liam Duckett <liamduckett2@gmail.com>
2024-02-24 19:05:39 +0000

    add medium and hard grids for further testing

--- app/Livewire/Game.php
+++ app/Livewire/Game.php
@@ -12,6 +12,7 @@ class Game extends Component
 
     public function mount(): void
     {
+        // easy
         $grid = [
             [null, null, 4, null, null, 7, 8, 3, null],
             [null, 1, null, 9, 3, null, 7, null, 5],
@@ -23,6 +24,32 @@ class Game extends Component
             [2, 6, null, 1, null, null, null, 7, 8],
             [5, null, 1, 6, null, null, 4, null, null],
         ];
+
+        // medium
+        $grid = [
+            [null, null, null, 4, null, 7, 5, null, null],
+            [null, 7, null, null, null, 5, null, null, 2],
+            [5, 4, null, 6, null, null, null, null, 7],
+            [null, null, null, null, 6, null, 2, 1, null,],
+            [6, 2, null, 5, 9, 8, 7, null, null],
+            [null, 9, null, null, null, null, 6, null, null],
+            [null, 5, null, null, 7, 1, 9, null, null],
+            [null, null, null, null, null, 9, 8, null, null],
+            [null, null, 8, null, null, 6, null, null, 3],
+        ];
+
+        // hard
+        //$grid = [
+        //    [null, null, null, null, null, null, null, null, null],
+        //    [8, null, null, null, null, 7, 2, null, null],
+        //    [null, null, 5, null, null, 1, null, 3, null],
+        //    [null, 5, null, null, 1, 9, 8, null, null],
+        //    [null, null, 6, null, null, null, 9, null, null],
+        //    [null, null, 3, null, null, 2, 1, null, null],
+        //    [null, 6, null, 7, null, null, null, 2, null],
+        //    [null, null, 7, null, null, 5, null, 4, null],
+        //    [null, null, null, 9, 6, null, null, null, null],
+        //];
 
         $this->sudoku = Sudoku::setUp($grid);
     }
